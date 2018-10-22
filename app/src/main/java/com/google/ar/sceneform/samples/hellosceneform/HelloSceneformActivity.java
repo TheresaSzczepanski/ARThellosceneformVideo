c6fb7c3a675b2e9508766e803dbcb21de24cddb9
robertbrodin <43351978+robertbrodin@users.noreply.github.com>
2018-10-22 18:32:54 -0400

    Fixed bug with second record attempt

--- app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/HelloSceneformActivity.java
+++ app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/HelloSceneformActivity.java
@@ -61,11 +61,11 @@ import java.util.Date;
  * This is an example activity that uses the Sceneform UX package to make common AR tasks easier.
  */
 public class HelloSceneformActivity extends AppCompatActivity {
-  private static final String TAG = HelloSceneformActivity.class.getSimpleName();
-  private static final double MIN_OPENGL_VERSION = 3.0;
+    private static final String TAG = HelloSceneformActivity.class.getSimpleName();
+    private static final double MIN_OPENGL_VERSION = 3.0;
 
-  private ArFragment arFragment;
-  private ModelRenderable andyRenderable;
+    private ArFragment arFragment;
+    private ModelRenderable andyRenderable;
 
     private static final int PERMISSION_CODE = 1;
     private int mScreenDensity;
@@ -77,119 +77,122 @@ public class HelloSceneformActivity extends AppCompatActivity {
     private MediaProjection.Callback mMediaProjectionCallback;
     private ToggleButton mToggleButton;
     private MediaRecorder mMediaRecorder;
+    private Surface getSurface;
 
-  @Override
-  @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
-  // CompletableFuture requires api level 24
-  // FutureReturnValueIgnored is not valid
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    private int counter = 0;
 
-
-    if (!checkIsSupportedDeviceOrFinish(this)) {
-      return;
-    }
-
-    setContentView(R.layout.activity_ux);
-    arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+    @Override
+    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
+    // CompletableFuture requires api level 24
+    // FutureReturnValueIgnored is not valid
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
 
-      DisplayMetrics metrics = new DisplayMetrics();
-      getWindowManager().getDefaultDisplay().getMetrics(metrics);
-      mScreenDensity = metrics.densityDpi;
-
-      DISPLAY_HEIGHT = metrics.heightPixels;
-      DISPLAY_WIDTH = metrics.widthPixels;
-
-      initRecorder();
-      prepareRecorder();
-
-      mProjectionManager = (MediaProjectionManager) getSystemService
-              (Context.MEDIA_PROJECTION_SERVICE);
-
-      mToggleButton = (ToggleButton) findViewById(R.id.toggle);
-      mToggleButton.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-              onToggleScreenShare(v);
-          }
-      });
-
-      mMediaProjectionCallback = new MediaProjectionCallback();
-
-
-
-      // When you build a Renderable, Sceneform loads its resources in the background while returning
-    // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
-    ModelRenderable.builder()
-      .setSource(this, Uri.parse("andy.sfb"))
-            //In order to use a different object, check this source
-            // .setSource(this, R.raw.andy) is the other option for setting the image source.
-        .build()
-        .thenAccept(renderable -> andyRenderable = renderable)
-        .exceptionally(
-            throwable -> {
-              Toast toast =
-                  Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
-              toast.setGravity(Gravity.CENTER, 0, 0);
-              toast.show();
-              return null;
-            });
-
-    arFragment.setOnTapArPlaneListener(
-        (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
-          if (andyRenderable == null) {
+        if (!checkIsSupportedDeviceOrFinish(this)) {
             return;
-          }
+        }
 
-          // Create the Anchor.
-          Anchor anchor = hitResult.createAnchor();
-          AnchorNode anchorNode = new AnchorNode(anchor);
-          anchorNode.setParent(arFragment.getArSceneView().getScene());
+        setContentView(R.layout.activity_ux);
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
 
-          // Create the transformable andy and add it to the anchor.
-          TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
-          andy.setParent(anchorNode);
-          andy.setRenderable(andyRenderable);
-          andy.select();
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        mScreenDensity = metrics.densityDpi;
 
+        DISPLAY_HEIGHT = metrics.heightPixels;
+        DISPLAY_WIDTH = metrics.widthPixels;
 
+        mToggleButton = (ToggleButton) findViewById(R.id.toggle);
+        mToggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onToggleScreenShare(v);
+
+            }
         });
-  }
 
-  /**
-   * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
-   * on this device.
-   *
-   * <p>Sceneform requires Android N on the device as well as OpenGL 3.0 capabilities.
-   *
-   * <p>Finishes the activity if Sceneform can not run
-   */
-  public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
-    if (Build.VERSION.SDK_INT < VERSION_CODES.N) {
-      Log.e(TAG, "Sceneform requires Android N or later");
-      Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
-      activity.finish();
-      return false;
+        mMediaProjectionCallback = new MediaProjectionCallback();
+
+
+
+
+
+        // When you build a Renderable, Sceneform loads its resources in the background while returning
+        // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("andy.sfb"))
+                //In order to use a different object, check this source
+                // .setSource(this, R.raw.andy) is the other option for setting the image source.
+                .build()
+                .thenAccept(renderable -> andyRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast toast =
+                                    Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                            return null;
+                        });
+
+        arFragment.setOnTapArPlaneListener(
+                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+                    if (andyRenderable == null) {
+                        return;
+                    }
+
+                    // Create the Anchor.
+                    Anchor anchor = hitResult.createAnchor();
+                    AnchorNode anchorNode = new AnchorNode(anchor);
+                    anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                    // Create the transformable andy and add it to the anchor.
+                    TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
+                    andy.setParent(anchorNode);
+                    andy.setRenderable(andyRenderable);
+                    andy.select();
+
+
+                });
     }
-    String openGlVersionString =
-        ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
-            .getDeviceConfigurationInfo()
-            .getGlEsVersion();
-    if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
-      Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
-      Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
-          .show();
-      activity.finish();
-      return false;
+
+    /**
+     * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
+     * on this device.
+     *
+     * <p>Sceneform requires Android N on the device as well as OpenGL 3.0 capabilities.
+     *
+     * <p>Finishes the activity if Sceneform can not run
+     */
+
+    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        if (Build.VERSION.SDK_INT < VERSION_CODES.N) {
+            Log.e(TAG, "Sceneform requires Android N or later");
+            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+            activity.finish();
+            return false;
+        }
+        String openGlVersionString =
+                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            activity.finish();
+            return false;
+        }
+        return true;
     }
-    return true;
-  }
     @Override
     public void onDestroy() {
         super.onDestroy();
         if (mMediaProjection != null) {
             mMediaProjection.stop();
+            mMediaRecorder.reset();
+
             mMediaProjection = null;
         }
     }
@@ -214,11 +217,18 @@ public class HelloSceneformActivity extends AppCompatActivity {
 
     public void onToggleScreenShare(View view) {
         if (((ToggleButton) view).isChecked()) {
+            initRecorder();
+            prepareRecorder();
+            mProjectionManager = (MediaProjectionManager) getSystemService
+                    (Context.MEDIA_PROJECTION_SERVICE);
+
+
             mToggleButton.setBackgroundColor(Color.TRANSPARENT);
             mToggleButton.setText("   ");
             shareScreen();
+            counter = counter + 1;
         } else {
-            mMediaRecorder.stop();
+            //mMediaRecorder.stop();
             mMediaRecorder.reset();
             Log.v(TAG, "Recording Stopped");
             stopScreenSharing();
@@ -235,6 +245,7 @@ public class HelloSceneformActivity extends AppCompatActivity {
         }
         mVirtualDisplay = createVirtualDisplay();
         mMediaRecorder.start();
+
     }
 
     private void stopScreenSharing() {
@@ -242,6 +253,7 @@ public class HelloSceneformActivity extends AppCompatActivity {
             return;
         }
         mVirtualDisplay.release();
+        mMediaRecorder.reset();
         //mMediaRecorder.release();
     }
 
@@ -258,7 +270,7 @@ public class HelloSceneformActivity extends AppCompatActivity {
             if (mToggleButton.isChecked()) {
                 mToggleButton.setChecked(false);
                 mMediaRecorder.stop();
-                mMediaRecorder.reset();
+                //mMediaRecorder.reset();
                 Log.v(TAG, "Recording Stopped");
             }
             mMediaProjection = null;
@@ -277,7 +289,8 @@ public class HelloSceneformActivity extends AppCompatActivity {
     }
 
     public String getFilePath() {
-        final String directory = Environment.getExternalStorageDirectory() + File.separator + "Download";
+        // DCIM/Camera
+        final String directory = Environment.getExternalStorageDirectory() + File.separator + "Downloads";
         if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
             Toast.makeText(this, "Failed to get External Storage", Toast.LENGTH_SHORT).show();
             return null;
@@ -311,16 +324,20 @@ public class HelloSceneformActivity extends AppCompatActivity {
 
             if (mMediaRecorder == null) {
                 mMediaRecorder = new MediaRecorder();
-                mMediaRecorder.setVideoEncodingBitRate(10000000);
-                mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
-                mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-                mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.HEVC);
-                mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-                mMediaRecorder.setVideoFrameRate(30);
-                mMediaRecorder.setVideoSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
-                mMediaRecorder.setOutputFile(getFilePath());
             }
+            //CamcorderProfile cpHigh = CamcorderProfile.get(CamcorderProfile.QUALITY_1080P);
+
+            mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+            mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+            mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
+            mMediaRecorder.setVideoSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+            mMediaRecorder.setVideoFrameRate(30);
+            mMediaRecorder.setOutputFile(getFilePath());
+            mMediaRecorder.setVideoEncodingBitRate(10000000);
+            
+            //mMediaRecorder.setOutputFile(getFilePath());
         }
     }
 }
